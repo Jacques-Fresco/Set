6e17004695d4346fad78364052d9f2bc63ed7f8a
tvink_tvink2015_tvink@mail.ru <tvink_tvink2015_tvink@mail.ru>
2021-01-25 03:07:52 +1000

    Добавьте файлы проекта.

---
+++ Set/Model/EasySet.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set.Model
+{
+    public class EasySet<T> : IEnumerable
+    {
+        private List<T> items = new List<T>();
+        public int Count => items.Count;
+        public EasySet() { }
+        public EasySet(T data)
+        {
+            items.Add(data);
+        }
+        public EasySet(IEnumerable<T> items) 
+        {
+            this.items = items.ToList();
+        }
+        public void Add(T item)
+        {
+            if (items.Contains(item)) return;
+
+            /*foreach(T i in items)
+            {
+                if(i.Equals(item))
+                {
+                    return;
+                }
+            }*/
+
+            items.Add(item);
+        }
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
+
+        public EasySet<T> Union(EasySet<T> set)
+        {
+            //return new EasySet<T>(items.Union(set.items)); // с помощью linq (она лучше, так как оптимизирована внутри самого linq)
+
+            EasySet<T> result = new EasySet<T>();
+
+            foreach (T item in items) // Добавляем в коллекцию все элементы из первой
+            {
+                result.Add(item);
+            }
+
+            foreach (T item in set.items) // Добавляем в коллекцию те элементы, которых не было в впервой
+            {
+                result.Add(item); // проверка на наличие элемента производится в Add
+            }
+
+            return result;
+        }
+        public EasySet<T> Intersection(EasySet<T> set)
+        {
+            //return new EasySet<T>(items.Intersect(set.items)); // с помощью linq (она лучше, так как оптимизирована внутри самого linq)
+            EasySet<T> result = new EasySet<T>();
+
+            // Для оптимизации
+            // A = 1 2 5; B = 1 2 3 7 5;
+            // Если начинать перебирать с B, то => [1]1 + [2]2 + [3]3 + [7]3 + [5]3 = 12 проходов
+            // Если начинать перебирать с A, то => [1]1 + [2]2 + [5]5 = 8 проходов
+
+            {
+                EasySet<T> big;
+                EasySet<T> small;
+                if (Count >= set.Count)
+                {
+                    big = this;
+                    small = set;
+                }
+                else
+                {
+                    big = set;
+                    small = this;
+                }
+            }
+
+            foreach (T itemOne in items) // Добавляем в коллекцию все элементы из первой
+            {
+                foreach (T itemTwo in set.items)
+                {
+                    if (itemOne.Equals(itemTwo))
+                    {
+                        result.Add(itemOne);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+        public EasySet<T> Difference(EasySet<T> set)
+        {
+            EasySet<T> result = new EasySet<T>(items);
+
+            foreach(T item in set.items)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+        public bool Subset(EasySet<T> set)
+        {
+            // все элементы подмножества должны содержаться внутри базового множества
+            //return items.All(i => set.items.Contains(i)); // Если хотя бы один false? то всё false
+
+            foreach(T item1 in items)
+            {
+                bool equals = false;
+                foreach(T item2 in set.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        equals = true;
+                        break;
+                    }
+                }
+                if (!equals) return false;
+            }
+
+            return true;
+        }
+        public EasySet<T> SymmetricDifference(EasySet<T> set)
+        {
+            // ищем две разности, а потом их просто объединяем, в дальнейшем передавая полученное значение в качестве параметра в конструктор
+            //return new EasySet<T>(items.Except(set.items).Union(set.items.Except(items)));
+
+            EasySet<T> result = new EasySet<T>();
+
+            foreach (T item1 in items)
+            {
+                bool flag = false;
+                foreach(T item2 in set.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag) result.Add(item1);
+            }
+
+            foreach (T item1 in set.items)
+            {
+                bool flag = false;
+                foreach (T item2 in items)
+                {
+                    if (item1.Equals(item2))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag) result.Add(item1);
+            }
+
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
---
+++ Set/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Model.EasySet<int> one = new Model.EasySet<int>(1);
+            one.Add(2);
+            one.Add(3);
+            one.Add(4);
+            one.Add(5);
+
+            Model.EasySet<int> two = new Model.EasySet<int>(4);
+            two.Add(5);
+            two.Add(6);
+            two.Add(7);
+            two.Add(8);
+
+            Model.EasySet<int> three = one.Union(two);
+
+            foreach(int item in three)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            Model.EasySet<int> easyset1 = new Model.EasySet<int>(new int[] { 1, 2, 3, 4, 5 });
+            Model.EasySet<int> easyset2 = new Model.EasySet<int>(new int[] { 4, 5, 6, 7, 8 });
+            Model.EasySet<int> easyset3 = new Model.EasySet<int>(new int[] { 3, 4, 5 });
+
+            Console.WriteLine("Union: ");
+            foreach(Object item in easyset1.Union(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nIntersection: ");
+            foreach (Object item in easyset1.Intersection(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nDifference A \\ B: ");
+            foreach (Object item in easyset1.Difference(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nDifference B \\ A: ");
+            foreach (Object item in easyset2.Difference(easyset1))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nA Subset C: ");
+            Console.Write(easyset1.Subset(easyset3));
+            Console.WriteLine();
+            
+            Console.WriteLine("\nC Subset A: ");
+            Console.Write(easyset3.Subset(easyset1));
+            Console.WriteLine();
+
+            Console.WriteLine("\nC Subset B: ");
+            Console.Write(easyset3.Subset(easyset2));
+            Console.WriteLine();
+
+            Console.WriteLine("\nSymmetricDifference: ");
+            foreach (Object item in easyset1.SymmetricDifference(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
